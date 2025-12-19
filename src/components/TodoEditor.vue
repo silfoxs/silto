6c2a1bb3e12073d45a159b0b137eafe4d9e76522
silfoxs <songwenwen@outlook.com>
2025-12-19 16:51:43 +0800

    update del confirm dig

--- src/components/TodoEditor.vue
+++ src/components/TodoEditor.vue
@@ -31,7 +31,7 @@ const handleClose = () => {
 <template>
   <DialogRoot :open="open" @update:open="emit('update:open', $event)">
     <DialogPortal to="#app-portal">
-      <DialogOverlay class="fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
+      <DialogOverlay class="fixed inset-0 z-50 bg-transparent data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
       <DialogContent class="fixed bottom-4 left-4 right-4 z-50 grid gap-2 border border-white/40 bg-white dark:bg-black/40 backdrop-blur-2xl dark:backdrop-saturate-[1.5] p-4 shadow-2xl duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:slide-out-to-bottom-[48%] data-[state=open]:slide-in-from-bottom-[48%] rounded-3xl">
         <div class="flex items-center justify-center">
           <DialogTitle class="text-base font-semibold text-gray-900 dark:text-white">
